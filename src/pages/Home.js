935e3a6766a67890d6af507df39c6117d180b2c0
Roald Yen <roald112100@gmail.com>
2022-10-02 00:28:18 +0800

    Adjusted Margin for texts in Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,14 +10,13 @@ function Home() {
       style={{backgroundImage: `url(${BannerImage})`}}
     >
       <div className="headerContainer">
-        <h1>Hi! I'm Roald.</h1>
-        <p>UI/UX Design and Front-End Development.</p>
+        <h1 className="headerName">Hi! I'm Roald.</h1>
+        <p classname="headerDesc">UI/UX Design and Front-End Development.</p>
         <Link to="/about">
-          <button>View My Profile</button>
+          <button className="btnProfile">View My Profile</button>
         </Link>
       </div>
     </div>
   )
 }
-
 export default Home;
